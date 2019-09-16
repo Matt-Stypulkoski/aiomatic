bea2e043c14db549077588c660ef17dc9d4e848c
Andrew Bennett <andrew.bennett@ibm.com>
2019-09-16 13:34:42 -0400

    dockerfile changes to remote branch

--- aiomaticProject/views.py
+++ aiomaticProject/views.py
@@ -2,4 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. Let's start our AI-o-matic project")
+	print('data is being loaded')
+	return HttpResponse("Hello, world. Let's start our AI-o-matic project")
+
+
