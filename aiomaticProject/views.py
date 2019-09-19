c7323353f517434b50320def97f22f0d6fa33d47
Matthew Stypulkoski <Matthew.Stypulkoski@ibm.com>
2019-09-19 16:10:41 -0400

    Add api view

--- aiomaticProject/views.py
+++ aiomaticProject/views.py
@@ -1,5 +1,10 @@
 from django.http import HttpResponse
+from .api import create_trained_model
 
 
 def index(request):
     return HttpResponse("Hello, world. Let's start our AI-o-matic project")
+
+
+def api(request):
+    return create_trained_model(request)
